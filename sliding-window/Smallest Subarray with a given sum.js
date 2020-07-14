d6ef8bca78c179566797747819d857403c970e67
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-07-14 08:25:33 +0530

    Update Smallest Subarray with a given sum.js

--- sliding-window/Smallest Subarray with a given sum.js
+++ sliding-window/Smallest Subarray with a given sum.js
@@ -34,3 +34,11 @@ console.log(
     2
   ])}`
 );
+
+/**
+Time Complexity #
+The time complexity of the above algorithm will be O(N)O(N). The outer for loop runs for all elements and the inner while loop processes each element only once, therefore the time complexity of the algorithm will be O(N+N)O(N+N) which is asymptotically equivalent to O(N)O(N).
+
+Space Complexity #
+The algorithm runs in constant space O(1)O(1).
+**/
