62b9916feb3bd40417fd48c39669f175e34f9c6a
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-07-15 22:04:17 +0530

    Create findAnagramPatterns.js

---
+++ sliding-window/findAnagramPatterns.js
@@ -0,0 +1,72 @@
+/**
+Given a string and a pattern, find all anagrams of the pattern in the given string.
+
+Anagram is actually a Permutation of a string. For example, “abc” has the following six anagrams:
+
+abc
+acb
+bac
+bca
+cab
+cba
+Write a function to return a list of starting indices of the anagrams of the pattern in the given string.
+
+Example 1:
+
+Input: String="ppqp", Pattern="pq"
+Output: [1, 2]
+Explanation: The two anagrams of the pattern in the given string are "pq" and "qp".
+Example 2:
+
+Input: String="abbcabc", Pattern="abc"
+Output: [2, 3, 4]
+Explanation: The three anagrams of the pattern in the given string are "bca", "cab", and "abc".
+**/
+
+function find_string_anagrams(str, pattern) {
+  let windowStart = 0,
+    matched = 0,
+    charFrequency = {};
+  var result = [];
+
+  for (var i = 0; i < pattern.length; i++) {
+    const chr = pattern[i];
+    if (!(chr in charFrequency)) {
+      charFrequency[chr] = 0;
+    }
+    charFrequency[chr] += 1;
+  }
+
+  // Our goal is to match all the characters from the 'charFrequency' with the current window
+  // try to extend the range [windowStart, windowEnd]
+  for (var windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    const rightChar = str[windowEnd];
+    if (rightChar in charFrequency) {
+      // Decrement the frequency of matched character
+      charFrequency[rightChar] -= 1;
+      if (charFrequency[rightChar] === 0) {
+        matched += 1;
+      }
+    }
+
+    if (matched === Object.keys(charFrequency).length) {
+      result.push(windowStart);
+    }
+
+    // Shrink the sliding window
+    if (windowEnd >= pattern.length - 1) {
+      var leftChar = str[windowStart];
+      windowStart += 1;
+      if (leftChar in charFrequency) {
+        if (charFrequency[leftChar] === 0) {
+          matched -= 1;
+        }
+        charFrequency[leftChar] += 1;
+      }
+    }
+  }
+  return result;
+}
+
+console.log(find_string_anagrams("ppqp", "pq"));
+console.log(find_string_anagrams("abbcabc", "abc"));
