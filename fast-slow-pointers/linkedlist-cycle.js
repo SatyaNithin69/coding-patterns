f70510c2d91046740f79b1106144667dbab8fa3a
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-07-18 17:05:27 +0530

    Create linkedlist-cycle.js

---
+++ fast-slow-pointers/linkedlist-cycle.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+function has_cycle(head) {
+  let slow = head;
+  let fast = head;
+
+  while (slow !== null && fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+
+head.next.next.next.next.next.next = head.next.next;
+console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`LinkedList has cycle: ${has_cycle(head)}`);
