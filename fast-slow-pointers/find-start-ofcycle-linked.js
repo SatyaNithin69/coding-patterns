b1052c78a261bec54c7f8d79910fb377ca8fd7bc
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-07-18 19:39:37 +0530

    Create find-start-ofcycle-linked.js

---
+++ fast-slow-pointers/find-start-ofcycle-linked.js
@@ -0,0 +1,42 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+function find_cycle_length(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      //cycle_length = calculate_cycle_length(slow);
+      return find_start(head, fast);
+    }
+  }
+}
+
+function find_start(head, fast) {
+  let pointer1 = head;
+  let pointer2 = fast;
+  while (pointer1 !== pointer2) {
+    pointer1 = pointer1.next;
+    pointer2 = pointer2.next;
+  }
+  return pointer1.data;
+}
+
+const head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+head.next.next.next.next.next.next = head.next.next;
+console.log(`LinkedList cycle length: ${find_cycle_length(head)}`);
+
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`LinkedList cycle length: ${find_cycle_length(head)}`);
