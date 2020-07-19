8e4e13cbe5ec08c2305573cb4d57f670503641fa
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-07-19 22:35:43 +0530

    Update merge-intervals.js

--- Merge-Intervals/merge-intervals.js
+++ Merge-Intervals/merge-intervals.js
@@ -31,3 +31,15 @@ let result = merge([
   new Interval(7, 9)
 ]);
 console.log(result);
+
+/**
+Time complexity #
+The time complexity of the above algorithm is O(N * logN)O(N∗logN), where ‘N’ is the total number of intervals.
+We are iterating the intervals only once which will take O(N)O(N), in the beginning though, since we need to sort the intervals,
+our algorithm will take O(N * logN)O(N∗logN).
+
+Space complexity #
+The space complexity of the above algorithm will be O(N)O(N) as we need to return a list containing all the merged intervals.
+We will also need O(N)O(N) space for sorting. For Java, depending on its version, Collection.sort() either uses Merge sort or Timsort, 
+and both these algorithms need O(N)O(N) space. Overall, our algorithm has a space complexity of O(N).
+**/
