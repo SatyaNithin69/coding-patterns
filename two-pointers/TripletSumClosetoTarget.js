bd747fba2f1e8b06a2d449a861b37072b05c9b80
palakurthiprashanth <31988005+palakurthiprashanth@users.noreply.github.com>
2020-07-28 20:11:25 +0530

    Update TripletSumClosetoTarget.js

--- two-pointers/TripletSumClosetoTarget.js
+++ two-pointers/TripletSumClosetoTarget.js
@@ -21,3 +21,44 @@ https://www.youtube.com/watch?v=_diMDpuEYBM
 
 
 **/
+
+function triplet_sum_close_to_target(arr, targetSum) {
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+  let min = Number.POSITIVE_INFINITY;
+  for (var i = 0; i < arr.length - 2; i++) {
+    let start = i + 1;
+    let end = arr.length - 1;
+
+    let currentSum = arr[start] + arr[end] + arr[i];
+    while (start <= end) {
+      if (currentSum < targetSum) {
+        start += 1;
+      }
+      if (currentSum > targetSum) {
+        end -= 1;
+      }
+      if (currentSum === targetSum) {
+        start += 1;
+        end -= 1;
+      }
+      min = Math.min(min, Math.abs(targetSum - currentSum));
+    }
+  }
+  return targetSum - min;
+}
+console.log(triplet_sum_close_to_target([-2, 0, 1, 2], 2));
+console.log(triplet_sum_close_to_target([-3, -1, 1, 2], 1));
+console.log(triplet_sum_close_to_target([1, 0, 1, 1], 100));
+
+/**
+Time complexity #
+Sorting the array will take O(N* logN)O(N∗logN). Overall searchTriplet() will take O(N * logN + N^2)O(N∗logN+N​2
+​​ ), which is asymptotically equivalent to O(N^2)O(N
+​2
+​​ ).
+
+Space complexity #
+The space complexity of the above algorithm will be O(N)O(N) which is required for sorting.
+**/
